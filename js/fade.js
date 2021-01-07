d66be118bf4fcd5cd0738c48d90eabfd0aa1329a
evangel <evangeldaping@gmail.com>
2021-01-08 06:29:33 +0800

    responsive - mobile and Ipad

--- js/fade.js
+++ js/fade.js
@@ -7,11 +7,11 @@ window.addEventListener("scroll", () => {
   } else {
 	opacity = 0;
   }
-  document.querySelector(".artist").style.opacity = opacity;
-  document.querySelector(".artist_name").style.opacity = opacity;
-  document.querySelector(".facebook").style.opacity = opacity;
-  document.querySelector(".youtube").style.opacity = opacity;
-  document.querySelector(".instagram").style.opacity = opacity;
-  document.querySelector(".snapcatch").style.opacity = opacity;
-  document.querySelector(".twitter").style.opacity = opacity;
+  document.querySelector("#artist").style.opacity = opacity;
+  document.querySelector("#artist_name").style.opacity = opacity;
+  document.querySelector("#facebook").style.opacity = opacity;
+  document.querySelector("#youtube").style.opacity = opacity;
+  document.querySelector("#instagram").style.opacity = opacity;
+  document.querySelector("#snapcatch").style.opacity = opacity;
+  document.querySelector("#twitter").style.opacity = opacity;
 });
